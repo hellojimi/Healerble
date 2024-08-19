b92f0f950b72c778cfd77ace3fe9398082ca8312
Jimin Lee <rjsb0648@gmail.com>
2024-08-19 23:57:32 +0900

    Initial commit

---
+++ src/main/java/com/zero/healerble/HealerbleApplication.java
@@ -0,0 +1,13 @@
+package com.zero.healerble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealerbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealerbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/healerble/HealerbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.healerble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealerbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
