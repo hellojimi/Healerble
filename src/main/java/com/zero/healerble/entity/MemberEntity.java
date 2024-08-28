6df473b10ac006884f8b3d2c61a7edf019524cf5
Jimin Lee <rjsb0648@gmail.com>
2024-08-29 01:49:10 +0900

    chore: 회원가입/로그인

--- src/main/java/com/zero/healerble/entity/MemberEntity.java
+++ src/main/java/com/zero/healerble/entity/MemberEntity.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
